44e0b4e359dc907b4608c022f40655ad72201119
mrhopko <therealchrishopkinson@hotmail.com>
2018-03-31 20:24:33 +0100

    uploaddeck

--- cardgame.go
+++ cardgame.go
@@ -1,8 +1,12 @@
 package cardgame
 
 import (
+	"bytes"
+	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -25,6 +29,7 @@ func init() {
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/", handler).Methods("GET")
+	r.HandleFunc("/upload", ReceiveFile).Methods("POST")
 	return r
 }
 
@@ -45,7 +50,32 @@ func populateTemplates() map[string]*template.Template {
 	const viewPath = "view/"
 	t := make(map[string]*template.Template)
 	layout := template.Must(template.ParseFiles(viewPath + "layout.html"))
-	template.Must(layout.ParseFiles(viewPath + "cardhand.html"))
+	template.Must(layout.ParseFiles(viewPath+"cardhand.html", viewPath+"uploaddeck.html"))
 	t["layout"] = layout
 	return t
+}
+
+func ReceiveFile(w http.ResponseWriter, r *http.Request) {
+	var Buf bytes.Buffer
+	// in your case file would be fileupload
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	name := strings.Split(header.Filename, ".")
+	fmt.Printf("File name %s\n", name[0])
+	// Copy the file data to my buffer
+	io.Copy(&Buf, file)
+	// do something with the contents...
+	// I normally have a struct defined and unmarshal into a struct, but this will
+	// work as an example
+	contents := Buf.String()
+	fmt.Println(contents)
+	// I reset the buffer in case I want to use it again
+	// reduces memory allocations in more intense projects
+	Buf.Reset()
+	// do something else
+	// etc write header
+	return
 }
